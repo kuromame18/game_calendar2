5da7c9b409e58bfba1e04989117ef137674f281e
Hayasaka <146614468+kuromame18@users.noreply.github.com>
2024-01-24 15:46:10 +0000

    deviseインストール　Topページルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  devise_for :admin, controllers: {
+    sessions: "admin/sessions"
+  }
+  scope module: :public do
+    root to: "homes#top"
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
