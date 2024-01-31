b47d09f5876ed7886bceed2b7611733a28893ecf
Hayasaka <146614468+kuromame18@users.noreply.github.com>
2024-01-31 14:44:56 +0000

    Topページに説明追加

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -1,7 +1,19 @@
+// sp-menu
 $(function() {
   $('.menu-trigger').on('click', function(event) {
     $(this).toggleClass('active');
     $('#sp-menu').fadeToggle();
+    event.preventDefault();
+  });
+});
+
+// topページ
+$(function () {
+  $('.top-start-btn').on('click',function(event){
+    var target = $(this).attr('href');
+    $('body, html').animate({
+      scrollTop: $(target).offset().top
+    },600);
     event.preventDefault();
   });
 });
