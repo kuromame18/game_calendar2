3bad917b0bf0510a09952aef5ba09aad2752d084
Hayasaka <146614468+kuromame18@users.noreply.github.com>
2024-02-04 07:56:30 +0000

    explainにアニメーション追加

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -17,3 +17,23 @@ $(function () {
     event.preventDefault();
   });
 });
+
+// explain
+document.addEventListener('DOMContentLoaded', function () {
+  var animatedElement = document.querySelector('#explain');
+  window.addEventListener('scroll', function() {
+    if (isElementInViewport(animatedElement)) {
+      animatedElement.classList.add('active');
+    };
+  });
+});
+
+function isElementInViewport(element) {
+  var rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
